510349f297347bd4fbf039d92c7bea7eb075cc3f
VietAnh219 <phungvietanh21092003@gmail.com>
2023-03-14 15:12:46 +0700

    commit note

---
+++ BaiThucHanhLab2/src/Bai3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.jar.Attributes.Name;
+
+public class Bai3 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int year, yearold;
+        String Name;
+
+        System.out.println("Nhap ten cua ban: ");
+        Name = scanner.nextLine();
+
+        System.out.println("Nhap vao nam sinh cua ban: ");
+        year = scanner.nextInt();
+
+        yearold = 2023 - year;
+
+        if (yearold < 16) {
+            System.out.println("Ban " + Name + " o do tuoi vi thanh nien");
+        } else if (yearold >= 16 && yearold < 18) {
+            System.out.println("Ban " + Name + " o do tuoi truong thanh");
+        } else {
+            System.out.println("Ban " + Name + " da gia");
+        }
+
+    }
+}
